b9362bb5fece6c14aa6bde8f8f47272e4db753f5
GregoMur <GregoMur@yandex.ru>
2023-07-27 19:49:17 +0300

    feat: create index.js for components

---
+++ src/components/index.js
@@ -0,0 +1,43 @@
+import App from "./App/App";
+import AboutMe from './AboutMe/AboutMe';
+import AboutProject from './AboutProject/AboutProject';
+import FilterCheckbox from './FilterCheckbox/FilterCheckbox';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Login from './Login/Login';
+import Main from './Main/Main';
+import Movies from './Movies/Movies';
+import MoviesCard from './MoviesCard/MoviesCard';
+import MoviesCardList from './MoviesCardList/MoviesCardList';
+import Navigation from './Navigation/Navigation';
+import NavTab from './NavTab/NavTab';
+import Portfolio from './Portfolio/Portfolio';
+import Promo from './Promo/Promo';
+import Register from './Register/Register';
+import SavedMovies from './SavedMovies/SavedMovies';
+import SearchFrom from './SearchForm/SearchForm';
+import Techs from './Techs/Techs';
+import NotFoundPage from './NotFoundPage/NotFoundPage';
+
+export {
+  App,
+  AboutMe,
+  AboutProject,
+  FilterCheckbox,
+  Footer,
+  Header,
+  Login,
+  Main,
+  Movies,
+  MoviesCard,
+  MoviesCardList,
+  Navigation,
+  NavTab,
+  Portfolio,
+  Promo,
+  Register,
+  SavedMovies,
+  SearchFrom,
+  Techs,
+  NotFoundPage,
+};
