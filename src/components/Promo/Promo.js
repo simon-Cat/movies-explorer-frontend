bda8d9e07d1e7c5550c35fd82c24529c01b6325e
GregoMur <GregoMur@yandex.ru>
2023-07-28 22:00:21 +0300

    feat: create component Promo

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,19 @@
+import './Promo.css';
+import promo_img from '../../images/web_landing-logo.png';
+
+const Promo = () => {
+  return (
+    <section className='promo-container'>
+      <div className='promo-container__content'>
+        <div className='promo-container__description'>
+          <h1 className='promo-container__title'>Учебный проект студента факультета Веб-разработки.</h1>
+          <p className='promo-container__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        </div>
+        <img src={promo_img} alt='Promo' />
+      </div>
+      <button className='promo-container__button'>Узнать больше</button>
+    </section>
+  );
+};
+
+export default Promo;
