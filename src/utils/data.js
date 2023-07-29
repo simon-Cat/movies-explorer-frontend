8937b1061839555f44ab0e351971eae0c9c9e890
GregoMur <GregoMur@yandex.ru>
2023-07-30 00:36:03 +0300

    feat: replace data for components into a single file

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+// data for Techs
+export const technologies = ['HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoDB'];
+
+// data for Portfolio
+export const portfolio = [
+  {
+    title: 'Статичный сайт',
+    url: 'https://github.com/simon-Cat/how-to-learn',
+  },
+  {
+    title: 'Адаптивный сайт',
+    url: 'https://github.com/simon-Cat/russian-travel',
+  },
+  {
+    title: 'Одностраничное приложение',
+    url: 'https://github.com/simon-Cat/react-mesto-api-full-gha',
+  },
+];
+
+// data for Footer
+export const links = [
+  {
+    title: 'Яндекс.Практикум',
+    url: 'https://practicum.yandex.ru/',
+  },
+  {
+    title: 'Github',
+    url: 'https://github.com/simon-Cat'
+  }
+];
