782ffbeb62c4fe7aeb404e4bb4dadd22efe9523a
GregoMur <GregoMur@yandex.ru>
2023-07-30 20:56:21 +0300

    feat: add layout 768px for components Header and Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,7 +9,7 @@ const Promo = () => {
           <h1 className='promo-container__title'>Учебный проект студента факультета Веб-разработки.</h1>
           <p className='promo-container__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
         </div>
-        <img src={promo_img} alt='Promo' />
+        <img className='promo-container__image' src={promo_img} alt='Promo' />
       </div>
       <a className='promo-container__link' href='#about-project'>Узнать больше</a>
     </section>
