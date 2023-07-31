42d9a74d07cb69c19b914efafd70d193bd35b96f
GregoMur <GregoMur@yandex.ru>
2023-07-31 23:34:48 +0300

    feat: add gamburger menu

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,11 +1,26 @@
 import './NavTab.css';
 import { navLinks } from '../../utils/data';
+import { useState } from 'react';
 
 const NavTab = ({ externalClass, isSignin=false }) => {
+  // switch gamburger menu
+  const [ isGamburgerMenuOpen, setIsGamburgerMenuOpen ] = useState(false);
+
+  // gamburger menu switch handler
+  const switchGambugerMenuHandler = () => {
+    setIsGamburgerMenuOpen(!isGamburgerMenuOpen);
+  };
+
+
   return (
     <>
       { isSignin ? (
         <nav className='navigation-container navigation-container_type_authorized'>
+          <div onClick={switchGambugerMenuHandler} className={`navigation-container__gamburger-menu ${isGamburgerMenuOpen ? 'navigation-container__gamburger-menu_state_open' : 'navigation-container__gamburger-menu_state_close'}`}>
+            <div className='navigation-container__gamburger-menu-item'></div>
+            <div className='navigation-container__gamburger-menu-item'></div>
+            <div className='navigation-container__gamburger-menu-item'></div>
+          </div>
           <ul className='navigation-container__pages-links'>
             {navLinks.map((item, index) => (
               <li key={index}>
