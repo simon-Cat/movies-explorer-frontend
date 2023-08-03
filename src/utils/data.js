08f5e23c18c2ce986d8ab19778d0ad794cf862de
GregoMur <GregoMur@yandex.ru>
2023-08-03 21:11:11 +0300

    fix: correct data for movies

--- src/utils/data.js
+++ src/utils/data.js
@@ -40,3 +40,200 @@ export const navLinks = [
     url: '#',
   }
 ];
+
+// test data for movies
+export const movies = [
+  {
+    id: 1,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: true,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 2,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 3,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: true,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 4,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 5,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 6,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 7,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 8,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: true,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 9,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 10,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 11,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 12,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 13,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 14,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 15,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 16,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 17,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 18,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 19,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 20,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 21,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 22,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 23,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 24,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  }
+];
+
+// test data for saved movies
+export const savedMovies = [
+  {
+    id: 22,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 23,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  },
+  {
+    id: 24,
+    title: '33 слова о дизайне',
+    duration: '1ч42м',
+    like: false,
+    url: 'https://avatars.mds.yandex.net/i?id=48afb62a66698093f5d9268b5ddc1fe93b79d702-9040073-images-thumbs&n=13',
+  }
+];
