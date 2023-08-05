b5ad7c8b554bf5a63c0337a6f97bab159fb0ae4e
GregoMur <GregoMur@yandex.ru>
2023-08-05 14:10:54 +0300

    feat: add state, components savedMovies and Popup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,32 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Header, Main, Footer, NotFoundPage, Movies } from '../';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Header, Main, Footer, NotFoundPage, Movies, SavedMovies, Popup } from '../';
+import { useState } from 'react';
+import { movies, savedMovies } from '../../utils/data';
 
 function App() {
+
+  const location = useLocation();
+  const [ isPopupOpened, setIsPopupOpened ] = useState(false);
+
+  const handleSwitchPopupState = () => {
+    setIsPopupOpened(!isPopupOpened);
+  };
+
   return (
     <div className="App">
-      <Header externalClass='app__header-container' />
-      {/* <Main externalClass='app__main-container' /> */}
-      <Movies externalClass='app__movies-container' />
+      <Header externalClass='app__header-container' location={location} switchPopupStateHandler={handleSwitchPopupState} />
+        <Routes>
+          <Route path='/' element={<Main externalClass='app__main-container' />} />
+          <Route path='/movies' element={<Movies moviesCards={movies} externalClass='app__movies-container' />} />
+          <Route path='/saved-movies' element={<SavedMovies savedMoviesCards={savedMovies} externalClass='app__saved-movies-container' />} />
+          <Route path='/profile' />
+          <Route path='/signin' />
+          <Route path='/signup' />
+        </Routes>
       <Footer externalClass='app__footer-container' />
+      <Popup name='layer' isOpen={isPopupOpened} />
       {/* <NotFoundPage /> */}
-      <Routes>
-        <Route path='/' />
-        <Route path='/movies' />
-        <Route path='/saved-movies' />
-        <Route path='/profile' />
-        <Route path='/signin' />
-        <Route path='/signup' />
-      </Routes>
     </div>
   );
 }
