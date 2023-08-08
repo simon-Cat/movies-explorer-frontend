84c69dad048a05c8871e1a5ca1a46ff44ced4272
GregoMur <GregoMur@yandex.ru>
2023-08-08 23:02:29 +0300

    feat: add styles into NotFoundPage.css, add hook useNavigate

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,13 +1,20 @@
-import './NotFoundPage.css'
+import './NotFoundPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = () => {
+
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="not-found-container">
       <div className="not-found-container__content">
         <h1 className="not-found-container__title">404</h1>
         <p className="not-found-container__description">Страница не найдена</p>
       </div>
-      <button className="not-found-container__button">Назад</button>
+      <button onClick={goBack} className="not-found-container__button">Назад</button>
     </div>
   );
 };
