21bc578a45197a54888a992c4e405ea15fb54c21
GregoMur <GregoMur@yandex.ru>
2023-08-08 22:54:19 +0300

    feat: add styles into Header.css, add Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import './Header.css';
 import logo from '../../images/icons/logo-min.svg';
 import { Gamburger, NavTab, Navigation } from '../';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = ({ externalClass, location, switchPopupStateHandler, isSignin=true }) => {
   // nav tab state
@@ -14,9 +15,9 @@ const Header = ({ externalClass, location, switchPopupStateHandler, isSignin=tru
 
   return(
     <header className={`header-container ${externalClass} ${location.pathname === '/' ? 'header-container_bgcolor_pink' : 'header-container_bgcolor_white'}`}>
-      <a href='#' className='header-container__logo-link'>
+      <Link to='/' className='header-container__logo-link'>
         <img src={logo} alt='Logo' className='header-container__logo-image' />
-      </a>
+      </Link>
       <Gamburger changeGamburgerStateHandler={handleSwitchStateNavigation}  externalClass={`header-container__gamburger-menu`}/>
       <NavTab externalClass={`header-container__navigation`} />
       <Navigation closeNavigationHandler={handleSwitchStateNavigation} externalClass={`header__navigation-container ${ isNavigationOpen ? 'header__navigation-container_showed' : 'header__navigation-container_hidden' }`} />
