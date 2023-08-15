d4fba7adac385a3f44c41abfbe875ce9131f4a98
GregoMur <GregoMur@yandex.ru>
2023-08-15 13:42:56 +0300

    feat: add CurrentUserContext, save token into localStorage, add method onSignout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import {
   Header,
   Main,
@@ -13,85 +13,122 @@ import {
   Login,
   ProtectedRoute,
 } from '../';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { movies, savedMovies } from '../../utils/data';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import mainApi from '../../utils/MainApi';
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [curentUser, setCurrentUser] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isPopupOpened, setIsPopupOpened] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      Promise.all([mainApi.getUserInfo({
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      })])
+      .then(([{ name, email }]) => {
+        setCurrentUser({...curentUser, name, email});
+        navigate('/movies');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    } else return;
+  }, [isLoggedIn]);
 
   const handleSwitchPopupState = () => {
     setIsPopupOpened(!isPopupOpened);
   };
 
+  // signin
   const onLogin = (token) => {
     localStorage.setItem('token', token);
     setIsLoggedIn(true);
+  };
+
+  // signout
+  const onSignOut = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
   }
 
   return (
     <div className='App'>
-      {(location.pathname === '/' ||
-        location.pathname === '/movies' ||
-        location.pathname === '/saved-movies' ||
-        location.pathname === '/profile') && (
-        <Header
-          externalClass='app__header-container'
-          location={location}
-          switchPopupStateHandler={handleSwitchPopupState}
-          isLoggedIn={isLoggedIn}
-        />
-      )}
-      <main className='app__main-container'>
-        <Routes>
-          <Route path='*' element={<NotFoundPage />} />
-          <Route
-            path='/'
-            element={<Main externalClass='app__main-container' />}
+      <CurrentUserContext.Provider value={curentUser}>
+        {(location.pathname === '/' ||
+          location.pathname === '/movies' ||
+          location.pathname === '/saved-movies' ||
+          location.pathname === '/profile') && (
+          <Header
+            externalClass='app__header-container'
+            location={location}
+            switchPopupStateHandler={handleSwitchPopupState}
+            isLoggedIn={isLoggedIn}
           />
-          <Route
-            path='/movies'
-            element={
-              <ProtectedRoute
-                isLoggedIn={isLoggedIn}
-                element={Movies}
-                moviesCards={movies}
-              />
-            }
-          />
-          <Route
-            path='/saved-movies'
-            element={
-              <ProtectedRoute
-                isLoggedIn={isLoggedIn}
-                element={SavedMovies}
-                moviesCards={savedMovies}
-              />
-            }
-          />
-          <Route
-            path='/profile'
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn} element={Profile} />
-            }
-          />
-          <Route
-            path='/signin'
-            element={<Login externalClass='app__login-container' onLogin={onLogin} />}
-          />
-          <Route
-            path='/signup'
-            element={<Register externalClass='app__register-container' />}
-          />
-        </Routes>
-      </main>
-      {(location.pathname === '/' ||
-        location.pathname === '/movies' ||
-        location.pathname === '/saved-movies') && (
-        <Footer externalClass='app__footer-container' />
-      )}
-      <Popup name='layer' isOpen={isPopupOpened} />
+        )}
+        <main className='app__main-container'>
+          <Routes>
+            <Route path='*' element={<NotFoundPage />} />
+            <Route
+              path='/'
+              element={<Main externalClass='app__main-container' />}
+            />
+            <Route
+              path='/movies'
+              element={
+                <ProtectedRoute
+                  isLoggedIn={isLoggedIn}
+                  element={Movies}
+                  moviesCards={movies}
+                />
+              }
+            />
+            <Route
+              path='/saved-movies'
+              element={
+                <ProtectedRoute
+                  isLoggedIn={isLoggedIn}
+                  element={SavedMovies}
+                  moviesCards={savedMovies}
+                />
+              }
+            />
+            <Route
+              path='/profile'
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn} element={Profile} onSignOut={onSignOut} />
+              }
+            />
+            <Route
+              path='/signin'
+              element={
+                <Login externalClass='app__login-container' onLogin={onLogin} />
+              }
+            />
+            <Route
+              path='/signup'
+              element={<Register externalClass='app__register-container' />}
+            />
+          </Routes>
+        </main>
+        {(location.pathname === '/' ||
+          location.pathname === '/movies' ||
+          location.pathname === '/saved-movies') && (
+          <Footer externalClass='app__footer-container' />
+        )}
+        <Popup name='layer' isOpen={isPopupOpened} />
+      </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,23 +1,37 @@
 import './Profile.css';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import { useContext, useEffect, useState } from 'react';
 
-const Profile = ({ extrenalClass, name = 'Bob Marley', email = 'test@ya.ru' }) => {
+const Profile = ({ extrenalClass, onSignOut }) => {
+
+  const curentUser = useContext(CurrentUserContext);
+  const [ userInfo, setUserInfo ] = useState({});
+
+  useEffect(() => {
+    setUserInfo({...userInfo, name: curentUser.name || '', email: curentUser.email || ''});
+  }, [curentUser]);
+
+  const singOutHandler = () => {
+    onSignOut();
+  };
+
   return (
     <section className={`profile-container ${extrenalClass}`}>
-      <h2 className='profile-container__title'>Привет, {name}!</h2>
+      <h2 className='profile-container__title'>Привет, {userInfo.name}!</h2>
       <form name='profile-form' className='profile-container__form'>
         <ul className='profile-container__inputs-container'>
           <li className='profile-container__input-container'>
             <label htmlFor='profile-name' className='profile-container__input-label'>Имя</label>
-            <input id='profile-name' className='profile-container__input' placeholder='Введите имя' />
+            <input id='profile-name' className='profile-container__input' value={userInfo.name} placeholder='Введите имя' />
           </li>
           <li className='profile-container__input-container'>
             <label htmlFor='profile-email' className='profile-container__input-label'>E-mail</label>
-            <input id='profile-email' className='profile-container__input' placeholder='Введите email' />
+            <input id='profile-email' className='profile-container__input' value={userInfo.email} placeholder='Введите email' />
           </li>
         </ul>
 
         <button className='profile-container__form-button'>Редактировать</button>
-        <button className='profile-container__form-button profile-container__form-button_type_exit'>Выйти из аккаунта</button>
+        <button className='profile-container__form-button profile-container__form-button_type_exit' onClick={singOutHandler}>Выйти из аккаунта</button>
       </form>
     </section>
   );
