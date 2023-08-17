4c6108e473fbf56015453a921dc3e8fb54701e78
GregoMur <GregoMur@yandex.ru>
2023-08-17 13:42:00 +0300

    feat: add url for movies image

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,2 +1,3 @@
 export const MAIN_API_URL =  'http://localhost:3001';
 export const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+export const MOVIES_IMAGE_API_URL = 'https://api.nomoreparties.co';
