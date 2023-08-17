ec4ca79701d6aca66dbf6c2bb76daf9688e574a6
GregoMur <GregoMur@yandex.ru>
2023-08-17 13:47:36 +0300

    feat: add MOVIES_IMAGE_API_URL, getTimeFromMins, change value for movies card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,5 +1,7 @@
 import './MoviesCard.css';
 import { useLocation } from 'react-router-dom';
+import { MOVIES_IMAGE_API_URL } from '../../utils/baseUrls';
+import { getTimeFromMins } from '../../utils/getTimeFromMinutes';
 
 const MoviesCard = ({ card }) => {
 
@@ -7,16 +9,16 @@ const MoviesCard = ({ card }) => {
 
   return (
     <li className='movie-card'>
-      <img className='movie-card__image' alt={`Кадр из фильма "${card.title}"`} src={card.url} />
+      <img className='movie-card__image' alt={`Кадр из фильма "${card.nameRU}"`} src={`${MOVIES_IMAGE_API_URL}${card.image.url}`} />
       <div className='movie-card__description'>
-        <h2 className='movie-card__title'>{ card.title }</h2>
+        <a href={card.trailerLink} target='_blank' rel='noreferrer' className='movie-card__title'>{ card.nameRU }</a>
         {location.pathname === '/movies' ? (
           <button className={`movie-card__button ${card.like ? 'movie-card__button_type_filled-like' : 'movie-card__button_type__not-filled-like'}`} />
         ) : (
           <button className='movie-card__button movie-card__button_type_remove-saved-movie' />
         )}
       </div>
-      <p className='movie-card__duration'>{ card.duration }</p>
+      <p className='movie-card__duration'>{ getTimeFromMins(card.duration) }</p>
     </li>
   );
 };
