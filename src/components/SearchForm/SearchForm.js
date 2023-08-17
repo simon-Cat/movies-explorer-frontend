bcae60edc4e73d3cf2837645e108d682f1c59eb4
GregoMur <GregoMur@yandex.ru>
2023-08-17 16:06:29 +0300

    feat: add validation nd styles

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,34 +1,62 @@
-import "./SearchForm.css";
-import { FilterCheckbox } from "../";
+import './SearchForm.css';
+import { FilterCheckbox } from '../';
+import { useState } from 'react';
 
 const SearchForm = ({ onSearchMovies }) => {
+  // if value is not passed, show message
+  const [showMessage, setShowMessage] = useState(false);
+  // input's value
+  const [searchInputValue, setSearchInputValue] = useState('');
 
   const submitHandler = (e) => {
     e.preventDefault();
-    onSearchMovies();
-  }
+
+    if (!searchInputValue) {
+      setShowMessage(true);
+      return;
+    } else {
+      setShowMessage(false);
+      onSearchMovies();
+    }
+  };
+
+  const changeHandler = (e) => {
+    const value = e.target.value;
+    setSearchInputValue(value);
+  };
 
   return (
-    <form className="search-form-container" onSubmit={(e) => {submitHandler(e)}}>
-      <div className="search-form-container__search-panel">
-        <div className="search-form-container__input-panel">
+    <form
+      className='search-form-container'
+      onSubmit={(e) => {
+        submitHandler(e);
+      }}
+    >
+      <span className='search-form-container__input-message'>
+        {showMessage ? 'Нужно ввести ключевое слово' : ''}
+      </span>
+      <div className='search-form-container__search-panel'>
+        <div className='search-form-container__input-panel'>
           <label
-            htmlFor="search-input"
-            className="search-form-container__input-icon"
+            htmlFor='search-input'
+            className='search-form-container__input-icon'
           />
           <input
-            id="search-input"
-            type="text"
-            placeholder="Фильм"
-            className="search-form-container__input"
-            required
+            id='search-input'
+            type='text'
+            placeholder='Фильм'
+            value={searchInputValue}
+            className='search-form-container__input'
+            onInput={(e) => {
+              changeHandler(e);
+            }}
           />
-          <button className="search-form-container__button" />
-        </div>
-        <FilterCheckbox externalClass={"search-form-container__checkbox"} />
-      </div>
 
-      <hr className="search-form-container__bottom-border" />
+          <button className='search-form-container__button' />
+        </div>
+        <FilterCheckbox externalClass={'search-form-container__checkbox'} />
+      </div>
+      <hr className='search-form-container__bottom-border' />
     </form>
   );
 };
